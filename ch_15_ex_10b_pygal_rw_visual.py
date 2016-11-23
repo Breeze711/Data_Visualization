37e5825bf22da857a39970737ba6563b616cdd6f
Julie Stark <buttermilkcake@users.noreply.github.com>
2016-11-23 11:45:04 -0600

    Update ch_15_ex_10b_pygal_rw_visual.py

--- ch_15_ex_10b_pygal_rw_visual.py
+++ ch_15_ex_10b_pygal_rw_visual.py
@@ -1,25 +1,20 @@
 import pygal
 from pygal.style import LightSolarizedStyle
+from random import choice
 
-from random_walk import RandomWalk
 
-# Keep making new walks, as long as the program is active.
-while True:
-    #Make a random walk and plot the points.
-    rw = RandomWalk(50000)
-    rw.fill_walk()
-    
-    xy_chart = pygal.XY(height=10, width=6, stroke=True,
-                        style=LightSolarizedStyle, show_legend=True,
-                       human_readable=True, fill=True, show_dots=True,
-                       dot_size=4)                  
-    xy_chart.title = 'Random Walk'
-    xy_chart.add('rw.x_values', 'rw.y_values')
-    xy_chart.render()    
-                 
-    keep_running = input("Make another walk? (y/n):")
-    if keep_running == 'n':
-        break
+xy_chart = pygal.XY(height=10, width=6, stroke=True,
+                    style=LightSolarizedStyle, show_legend=True,
+                    human_readable=True, fill=True, show_dots=True,
+                    dot_size=4)                  
+xy_chart.title = 'Random Walk'
+xy_chart.x_labels = map(str, range(-2, 6))
+xy_chart.y_labels = map(str, range(-2, 6))
+xy_chart.add('x = choice(x)', [choice((-1, 1)) for x in range(0, 5)])
+xy_chart.add('x = num_points(x)', [num_points((50))])
+xy_chart.add('y = choice(y)', [choice((-1, 1)) for y in range(0, 5)])
+xy_chart.add('y = num_points(y)', [num_points((50))])                                   
+xy_chart.render_to_file('tues_test.svg')   
 
 
 
