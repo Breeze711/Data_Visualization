9a8d684a854bcbceef22f0cad8925ec399a366fa
Julie Stark <buttermilkcake@users.noreply.github.com>
2016-11-23 11:53:35 -0600

    Update ch_15_ex_10b_pygal_rw_visual.py

--- ch_15_ex_10b_pygal_rw_visual.py
+++ ch_15_ex_10b_pygal_rw_visual.py
@@ -11,9 +11,8 @@ xy_chart.title = 'Random Walk'
 xy_chart.x_labels = map(str, range(-2, 6))
 xy_chart.y_labels = map(str, range(-2, 6))
 xy_chart.add('x = choice(x)', [choice((-1, 1)) for x in range(0, 5)])
-xy_chart.add('x = num_points(x)', [num_points((50))])
 xy_chart.add('y = choice(y)', [choice((-1, 1)) for y in range(0, 5)])
-xy_chart.add('y = num_points(y)', [num_points((50))])                                   
+                  
 xy_chart.render_to_file('tues_test.svg')   
 
 
